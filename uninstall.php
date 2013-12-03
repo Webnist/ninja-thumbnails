efd7418ffe03a86c12c2634b1c2cabbccf887edb
Webnist <webnist@webnist.org>
2013-12-03 14:22:17 +0900

    Uninstall.php fixes

--- uninstall.php
+++ uninstall.php
@@ -4,3 +4,6 @@ if ( get_option( 'ninja_execution_date' ) )
 
 if ( get_option( 'ninja_onmitsu' ) )
 	delete_option( 'ninja_onmitsu' );
+
+if ( get_option( 'ninja_size' ) )
+	delete_option( 'ninja_size' );
